4b41d3a55f932470fd3838862fd6e9741d42e215
Salty-Byzrus <101959495+Salty-Byzrus@users.noreply.github.com>
2022-03-20 20:43:08 +0000

    Add files via upload

--- module/actor/roll-dice.js
+++ module/actor/roll-dice.js
@@ -34,8 +34,6 @@ export async  function rollDice(
   let dice = chanceDie ? 1 : parseInt(numDice) + healthModifier(wound);
   const roll = new Roll(dice + "dvcs>11 + " + 0 + "dhcs>11", actor.data.data);
   await roll.evaluate();
-  
-   // Variable defining
   let difficultyResult = "<span></span>";
   let success = 0;
   let critSuccess = 0;
@@ -44,8 +42,6 @@ export async  function rollDice(
   let hungerFail = 0;
   let hungerCritFail = 0;
   let chanceDieSuccess = false; 
-  
-  // Defines the normal diceroll results
   roll.terms[0].results.forEach((dice) => {
     if (numDice+healthModifier(wound) <= 0 && dice.result===10)
     { 
@@ -58,7 +54,7 @@ export async  function rollDice(
         critSuccess += 2;
       } else
       if (dice.result===1) {
-        success -= 2;
+        success==success-1
       }
       else {
         success++;
